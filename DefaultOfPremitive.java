02ce3243595ad817af142e6c948f69488b381930
Debasmitasutar <debasmita12.ds@gmail.com>
2023-06-29 22:23:44 +0530

    Added Default premitive value program

---
+++ DefaultOfPremitive.java
@@ -0,0 +1,34 @@
+package BasicJava;
+
+public class DefaultOfPremitive {
+      static int int1;
+     static char char1;
+     static String string1;
+    static boolean bool1;
+    static byte byte1;
+    static long long1;
+    static float float1;
+    static double double1;
+    static short short1;
+    public static void main(String[] args) {
+
+        //System.out.println("Default value of int-" +int1);
+
+        System.out.println("Default value of char-" +char1);
+        System.out.println("Default value of string-"+string1);
+        System.out.println("Default value of boolean-"+bool1);
+        System.out.println("Default value of byte-"+byte1);
+        System.out.println("Default value of long-"+long1);
+        System.out.println("Default value of float-"+float1);
+        System.out.println("Default value of double-"+double1);
+        System.out.println("Default value of short-"+short1);
+    }
+
+
+
+}
+
+
+
+
+
