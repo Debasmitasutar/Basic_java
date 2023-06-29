c5108cb346fba1efe2d305975bd1480fb8982f0d
Debasmitasutar <debasmita12.ds@gmail.com>
2023-06-29 22:27:33 +0530

    Added String Equal program

---
+++ StringsarEqualorNot.java
@@ -0,0 +1,14 @@
+package BasicJava;
+
+public class StringsarEqualorNot {
+    public static void main(String[] args) {
+        String name1="deba";
+        String name2="deba";
+        if (name1==name2){
+            System.out.println("Strings are equal!!");
+        }
+        else {
+            System.out.println("Strings are not equal!!");
+        }
+    }
+}
